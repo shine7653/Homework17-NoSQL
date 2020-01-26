8d607edd896bb3bfd6f7b8ed595053c78259efa6
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2020-01-26 12:34:13 -0800

    htmlRoutes

---
+++ routes/api-routes.js
@@ -0,0 +1,69 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our Todo model
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  app.get("/notes", (req, res) => {
+    db.Note.find({})
+      .then(dbNote => {
+        res.json(dbNote);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.get("/user", (req, res) => {
+    db.User.find({})
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.post("/submit", ({ body }, res) => {
+    db.Note.create(body)
+      .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.get("/populateduser", (req, res) => {
+    db.User.find({})
+      .populate("notes")
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  app.put("/populateduser", (req, res) => {
+    db.User.find({})
+      .populate("notes")
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+
+};
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+mongoose.connect(process.env.MONGODB_URI ||
+  "mongodb://localhost/nosql", { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Seeds
+// db.Workout.create({
+//   exercise: [
+//     {
+//       type: "resistance",
+//       name: "running",
+//       duration: 20,
+//       weight: 50,
+//       reps: 5,
+//       sets: 3,
+//       distance: 300
+//     }]
+// })
+//   .then(dbUser => {
+//     console.log(dbUser);
+//   })
+//   .catch(({ message }) => {
+//     console.log(message);
+//   });
+
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
