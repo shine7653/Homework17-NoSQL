b5110ed232f01a7bdd71bcf7d60e3aceacd6ced3
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2020-01-28 15:12:03 -0800

    almost

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,55 +16,101 @@ module.exports = function (app) {
     db.Workout.find({})
       .then(dbWorkout => {
         res.json(dbWorkout);
+        console.log(1);
       })
       .catch(err => {
         res.json(err);
+        console.log(2);
       });
   });
 
-  // app.get("/user", (req, res) => {
-  //   db.User.find({})
-  //     .then(dbUser => {
-  //       res.json(dbUser);
-  //     })
-  //     .catch(err => {
-  //       res.json(err);
-  //     });
-  // });
-
-  app.post("/api/workouts/", ({ body }, res) => {
-    db.Workout.create(body)
-      .then(({ _id }) =>
-        db.User.findOneAndUpdate({}, { $push: { Workouts: _id } }, { new: true }))
-      .then(dbUser => {
-        res.json(dbUser);
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+        console.log(3);
       })
       .catch(err => {
         res.json(err);
+        console.log(4);
       });
   });
 
-  // app.get("/populateduser", (req, res) => {
-  //   db.User.find({})
+  app.post("/api/workouts/", (req , res) => {
+    console.log("createWorkout", req.body);
+    db.Workout.create(req.body)
+    // .then(({ _id }) =>
+    //   db.Workout.create(
+    //     {req.},
+    //     // { $push: { Workout: _id } },
+    //     // { new: true }))
+    //   )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+      console.log(5, dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+      console.log(6);
+    });
+    
+    // const workout = {
+    //   exercise: req.body.exercise,
+    //   name: req.body.name,
+    //   duration: req.body.duration,
+    //   weight: req.body.weight,
+    //   reps: req.body.reps,
+    //   sets: req.body.sets,
+    //   distance: req.body.distance
+    // };
+    // const workout = new Workout({
+    //   _id: mongoose.Schema.Types.ObjectId(),
+    //   exercise: req.body.exercise,
+    //   name: req.body.name,
+    //   duration: req.body.duration,
+    //   weight: req.body.weight,
+    //   reps: req.body.reps,
+    //   sets: req.body.sets,
+    //   distance: req.body.distance
+    // });
+    // workout
+    // .save()
+    // .then(result => {
+    //   console.log(result);
+    // })
+    // .catch(err => console.log(err));
+
+    // -------------------------------
+
+
+  });
+
+  // app.get("/populatedWorkout", (req, res) => {
+  //   db.Workout.find({})
   //     .populate("Workouts")
-  //     .then(dbUser => {
-  //       res.json(dbUser);
+  //     .then(dbWorkout => {
+  //       res.json(dbWorkout);
   //     })
   //     .catch(err => {
   //       res.json(err);
   //     });
   // });
 
-  app.put("/api/workouts/", (req, res) => {
-    db.User.find({})
-      .populate("Workouts")
-      .then(dbUser => {
-        res.json(dbUser);
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.update(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } }
+    )
+      // .populate("Workouts")
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+        console.log(7);
       })
       .catch(err => {
         res.json(err);
+        console.log(8);
       });
   });
 
 
-};
+}
